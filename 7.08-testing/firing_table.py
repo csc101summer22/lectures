17785361f69eefbc7be043013cb9d96e4cb1e48c
cesiu <cesiu@calpoly.edu>
2022-07-08 08:55:22 -0700

    Added docstring and unittest examples.

--- 7.08-testing/firing_table.py
+++ 7.08-testing/firing_table.py
@@ -1,7 +1,52 @@
 # Computes trajectories of cannonballs.
 
 
+def get_float(prompt):
+    """
+    Get a floating point value from user input.
+
+    :param prompt: A string to show the user
+    :return: The next float the user types
+    """
+    return 0
+
+
+def time_of_flight(height):
+    """
+    Compute a cannonball's ideal time of flight.
+
+    :param height: A floating point initial height
+    :return: The floating point time of flight
+    """
+    return (2 * height / 9.81) ** 0.5
+
+
+def range(time, velocity):
+    """
+    Compute a cannonball's ideal range.
+
+    :param time: A floating point time of flight
+    :param velocity: A floating point horizontal velocity
+    :param range: The floating point distance traveled
+    """
+    # Initially, this will just be a "stub": the bare minimum code needed to
+    #  produce a syntactically valid function.
+    return 0
+
+
 def main():
+    # Information that we need from the user:
+    #   * How high is the cliff?
+    #   * How fast is the cannonball?
+    #   * How far away is the target?
+    # Information that we can then compute:
+    #   * How long is the cannonball in the air?
+    #   * How far does the cannonball travel?
+    # Based on these questions, a "wishlist" of functions:
+    #   * Function(s) to get input from the user
+    #   * Function to compute the time of flight
+    #   * Function to compute the range
+    print("Not done yet!")
 
 
 if __name__ == "__main__":
--- 7.08-testing/firing_table_tests.py
+++ 7.08-testing/firing_table_tests.py
@@ -1,9 +1,22 @@
 # Tests trajectories of cannonballs.
 
+# Unit tests can be written in Python using the unittest module:
 import unittest
+import firing_table
 
-
+# For now, just treat this as necessary boilerplate:
 class TestFiringTable(unittest.TestCase):
+    # Each test case is a function whose name starts with "test" and whose
+    #  lone parameter is "self":
+    def test01_time_of_flight(self):
+        time = firing_table.time_of_flight(50)
+        # Each test case should include at least one "assertion"; note that,
+        #  by default, floats will be checked to the seventh decimal place:
+        self.assertAlmostEqual(time, 3.1927543)
+
+    def test02_time_of_flight(self):
+        time = firing_table.time_of_flight(0)
+        self.assertAlmostEqual(time, 0)
 
 
 if __name__ == "__main__":
