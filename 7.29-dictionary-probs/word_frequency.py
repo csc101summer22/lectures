8e20587821d4f3f88abc2ca512d4cd602f9b9278
cesiu <cesiu@calpoly.edu>
2022-07-29 08:59:13 -0700

    Added dictionary problem example.

--- 7.29-dictionary-probs/word_frequency.py
+++ 7.29-dictionary-probs/word_frequency.py
@@ -4,22 +4,46 @@ import string
 def sanitize_word(word):
     """
     Remove non-alphabetical characters from a string.
+
+    :param word: An arbitrary string
+    :return: A lowercased string with punctuation and numbers removed
     """
-    pass
+    result = ""
+
+    for character in word.lower():
+        if character.isalpha():
+            result = result + character
+
+    return result
 
 
 def count_word(word, counts):
     """
     Count one occurrence of a word.
+
+    :param word: A sanitized string to count
+    :param counts: A dictionary mapping words to frequencies
     """
-    pass
+    if word not in counts and word != "":
+        counts[word] = 1
+    elif word != "":
+        counts[word] = counts[word] + 1
 
 
 def count_words(line):
     """
     Count all of the words in a line.
+
+    :param line: An arbitrary string of space-separated words
+    :return: A dictionary mapping words to frequencies
     """
-    pass
+    counts = {}
+
+    for word in line.split():
+        word = sanitize_word(word)
+        count_word(word, counts)
+
+    return counts
 
 
 def main():
--- 7.29-dictionary-probs/word_frequency_tests.py
+++ 7.29-dictionary-probs/word_frequency_tests.py
@@ -3,6 +3,44 @@ import word_frequency
 
 
 class TestWordFrequency(unittest.TestCase):
+    def test01_sanitize_word(self):
+        word = word_frequency.sanitize_word("hello")
+        self.assertEqual(word, "hello")
+
+    def test02_sanitize_word(self):
+        word = word_frequency.sanitize_word("Hello!")
+        self.assertEqual(word, "hello")
+
+    def test03_sanitize_word(self):
+        word = word_frequency.sanitize_word("%$^%$")
+        self.assertEqual(word, "")
+
+    def test04_count_word(self):
+        dct = {"you": 7, "i": 5, "and": 4}
+        word_frequency.count_word("you", dct)
+        self.assertEqual(dct["you"], 8)
+        self.assertEqual(dct["i"], 5)
+        self.assertEqual(dct["and"], 4)
+
+    def test05_count_word(self):
+        dct = {}
+        word_frequency.count_word("you", dct)
+        self.assertEqual(dct["you"], 1)
+        word_frequency.count_word("i", dct)
+        self.assertEqual(dct["i"], 1)
+
+    def test06_count_word(self):
+        dct = {"you": 7, "i": 5, "and": 4}
+        word_frequency.count_word("", dct)
+        self.assertEqual(dct["you"], 7)
+        self.assertEqual(dct["i"], 5)
+        self.assertEqual(dct["and"], 4)
+        self.assertFalse("" in dct)
+
+    def test07_count_words(self):
+        dct = word_frequency.count_words("Hello, world!")
+        self.assertEqual(dct["hello"], 1)
+        self.assertEqual(dct["world"], 1)
 
 
 if __name__ == "__main__":
